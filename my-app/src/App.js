a4c268cbda9f4ef4bac89b72718928b32b17ab9c
Steph Hjartarson <stephanie.hja@gmail.com>
2023-01-07 16:07:59 +0000

    Add event handlers to class + functional components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 // import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps"
 // import StatefulGreeting from './components/StatefulGreeting';
 // import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreetingWithPrevState />
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
