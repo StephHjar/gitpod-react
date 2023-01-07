b5eb9d6ac1b2493899cdc104e258c74779cdfeb1
Steph Hjartarson <stephanie.hja@gmail.com>
2023-01-07 17:21:37 +0000

    Update parent component from child using props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,12 +9,13 @@ import './App.css';
 // import EventBinding from './components/EventBinding';
 // import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 // import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
-import NestingComponents from './components/NestingComponents';
+// import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
   return (
     <div className="App">
-      <NestingComponents />
+      <MethodsAsPropsParent />
     </div>
   );
 }
