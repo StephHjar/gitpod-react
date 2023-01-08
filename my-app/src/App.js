9352858ffa178587703e75f5040efa35901f122a
Steph Hjartarson <stephanie.hja@gmail.com>
2023-01-08 14:43:41 +0000

    Convert controlled form to uncontrolled

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
-// import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps"
-// import StatefulGreeting from './components/StatefulGreeting';
-// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
-// import EventsFunctional from './components/EventsFunctional';
-// import EventsClass from './components/EventsClass';
-// import EventBinding from './components/EventBinding';
-// import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-// import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
-// import NestingComponents from './components/NestingComponents';
-// import MethodsAsPropsParent from './components/MethodsAsPropsParent';
-// import RenderingLists from './components/RenderingLists';
-// import LifeCyclesCDM from './components/LifeCyclesCDM';
-// import LifeCyclesCDU from './components/LifeCyclesCDU';
-// import LifeCyclesCWU from './components/LifeCyclesCWU';
-import ControlledForm from './components/ControlledForm';
+import UncontrolledForm from './components/UncontrolledForm';
 
 function App() {
   return (
     <div className="App">
-      <ControlledForm />
+      <UncontrolledForm />
     </div>
   );
 }
