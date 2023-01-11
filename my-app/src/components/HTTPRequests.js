ceac76648d897209302d0ec2649463247d30e0b6
Steph Hjartarson <stephanie.hja@gmail.com>
2023-01-11 21:06:09 +0000

    Create single component with GET & POST using hooks

--- my-app/src/components/HTTPRequests.js
+++ my-app/src/components/HTTPRequests.js
@@ -11,7 +11,7 @@ export class HTTPRequests extends Component {
         }
     }
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts/99999')
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
             .then(response => {
                 console.log(response);
                 this.setState({
